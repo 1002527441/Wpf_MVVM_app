0d34e91d603a445546661e166c01ab64b684176a
Henry.zhang <1002527441@qq.com>
2022-08-15 14:48:27 +0800

    init

---
+++ CommandBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public class CommandBase : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return DoCanExecute?.Invoke(parameter) == true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (!CanExecute(parameter)) return;
+            DoAction?.Invoke(parameter);
+        }
+
+
+        public void RaiseCanChanged()
+        {
+            CanExecuteChanged?.Invoke(this,new EventArgs());
+        }
+
+        public Action<object?> DoAction { get; set; } 
+
+        public Func<object?, bool> DoCanExecute { get; set; }
+    }
+}
---
+++ MainViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string  _value="Hello";
+
+        public string  Value
+        {
+            get { return _value; }
+            set {
+                    _value = value;
+                   PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Value"));
+                if (value == "100")
+                    ValueColor = Brushes.Red;
+
+
+                (this.ValueCommand as CommandBase).RaiseCanChanged();
+
+                }
+        }
+
+        private Brush _valueColor= Brushes.Orange;
+
+        public Brush ValueColor
+        {
+            get { return _valueColor; }
+            set { 
+                    _valueColor = value;
+                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ValueColor"));
+
+                }
+        }
+
+
+        private ICommand _valueCommand;
+
+        public ICommand ValueCommand
+        {
+            get { 
+                if (_valueCommand == null)
+                {
+                    _valueCommand = new CommandBase() {
+                        DoAction = new Action<object>(ValueCommandAction),
+                        DoCanExecute  = new Func<object?, bool>(CanExecute)
+                        
+                        };
+
+                }
+                return _valueCommand; }
+            set { 
+                _valueCommand = value;
+                }
+        }
+
+
+        private void ValueCommandAction(object obj)
+        {
+            Value = "100";
+        }
+
+        private bool CanExecute(object obj)
+        {
+            return !string.IsNullOrEmpty(Value);
+        }
+
+    }
+}
